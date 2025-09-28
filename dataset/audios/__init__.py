ff80543b9ac7b521264136c16e66555a3240295d
442401597@qq.com <442401597@qq.com>
2025-09-28 17:54:18 +0800

    feat(dataset): 添加SpoofCeleb数据集支持并修复ASVspoof2021数据集
    添加SpoofCelebDataset类并注册到数据集注册系统
    修复ASVspoof2021_LA_Dataset类的实现错误
    更新__init__.py导入新数据集类

--- dataset/audios/__init__.py
+++ dataset/audios/__init__.py
@@ -1,3 +1,5 @@
 # 音频数据集处理模块
 from .audio_dataset import load_audio_data_from_jsonl
 from .asvspoof2019_la import ASVspoof2019_LA_Dataset
+from .spoofceleb import SpoofCelebDataset
+from .asvspoof2021_la import ASVspoof2021_LA_Dataset
--- dataset/audios/asvspoof2021_la.py
+++ dataset/audios/asvspoof2021_la.py
@@ -4,10 +4,10 @@ from utils.audio_utils import load_audio, fix_audio_length
 from ..custom_datasets import custom_dataset_register
 
 
-class SpoofCelebDataset(Dataset):
+class ASVspoof2021_LA_Dataset(Dataset):
     def __init__(
         self, 
-        data_root: str = "/cache/datasets/spoofceleb/spoofceleb/",
+        data_root: str = "F:/datasets/ASVSpoof2021LA/",
         split: str = "eval",
         transform: callable = None,
         ):
@@ -15,19 +15,21 @@ class SpoofCelebDataset(Dataset):
         self.transform = transform
         self.split = split
         # 读取metadata
-        meta_csv_path = os.path.join(data_root, f"metadata/{split}.csv")
-        self.metadata = pd.read_csv(meta_csv_path)
+        suffix_map = {"eval": "trl"}
+        self.metadata = pd.read_csv(
+            f"{data_root}/ASVspoof2021_LA_cm_protocols/ASVspoof2021.LA.cm.{split}.{suffix_map[split]}.txt",
+             sep=" ", header=None)
         #
-        self.metadata["audio_path"] = f"{data_root}/flac/{split}/" + self.metadata["file"]
-        self.metadata["label"] = self.metadata["attack"]
+        self.metadata.columns = ["audio_name", "file_name", "_", "attack", "is_spoof"]
 
     def __len__(self):
         return len(self.metadata)
 
     def __getitem__(self, idx):
-        audio_path = self.metadata.loc[idx, "audio_path"]
-        label = self.metadata.loc[idx, "label"]
+        audio_name = self.metadata.loc[idx, "file_name"]
+        label = self.metadata.loc[idx, "is_spoof"]
         # 读取音频
+        audio_path = f"{self.data_root}/ASVspoof2019_LA_{self.split}/flac/{audio_name}.flac"
         waveform, sample_rate = load_audio(audio_path)
         # 固定音频长度
         waveform, sample_rate = fix_audio_length(waveform, sample_rate, target_length_sec=4)
@@ -50,4 +52,4 @@ class SpoofCelebDataset(Dataset):
         self.transform = transform_fn
 
 
-custom_dataset_register('SpoofCeleb', SpoofCelebDataset)
+custom_dataset_register('ASVSpoof2021LA', ASVspoof2021_LA_Dataset)
